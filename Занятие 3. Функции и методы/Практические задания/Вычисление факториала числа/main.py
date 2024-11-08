ef49890e1f6e95bf70de8e333c93c7b76dece58e
zoninfv <zonin.fv@gmail.com>
2024-11-08 21:23:47 +0300

    Commit 08.11 21:23 init

--- Занятие 3. Функции и методы/Практические задания/Вычисление факториала числа/main.py
+++ Занятие 3. Функции и методы/Практические задания/Вычисление факториала числа/main.py
@@ -1,6 +1,6 @@
 # TODO Запишите функцию `factorial`
 n = 0
-def factorial():
+def factorial(n):
     if n == 0:
         return 1
 
@@ -8,9 +8,8 @@ def factorial():
     for num in range(1, n + 1):
         result *= num
 
-        return result
+    return result
 
 # TODO Вызовите функцию factorial и распечатайте результат 
 print(f"Факториал числа 0 равен {factorial(0)}")
 print(f"Факториал числа 5 равен {factorial(5)}")
-print(factorial(5))
--- Занятие 3. Функции и методы/Практические задания/Построение лестницы/main.py
+++ Занятие 3. Функции и методы/Практические задания/Построение лестницы/main.py
@@ -1,4 +1,9 @@
-# TODO Напишите функцию print_stairs
+def print_stairs(count_stairs=4):
+    for n in range (1,count_stairs+1):
+        print ("*"* n)
+print_stairs())
+print_stairs(10))
+
 
 
 # TODO вызвать функцию без аргументов и с аргументом равным 10
